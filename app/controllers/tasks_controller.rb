a18d2afe7553ca9c8e952af7cf2945e9990603ae
Tomoshige Jimu <keririnkery.kery@gmail.com>
2023-02-25 21:25:39 +0900

    Create Validates(Chapter4:until_p144)

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,11 +3,21 @@ class TasksController < ApplicationController
     @tasks = Task.all
   end
 
-  def show
-    @task = Task.find(params[:id])
+  def new
+    @task = Task.new
   end
 
-  def new
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to @task, notice: "タスク「#{@task.name}」を登録しました。"
+    else
+      render :new
+    end
+  end
+
+  def show
+    @task = Task.find(params[:id])
   end
 
   def edit
@@ -24,16 +34,6 @@ class TasksController < ApplicationController
     task = Task.find(params[:id])
     task.destroy
     redirect_to tasks_url, notice: "タスク「#{task.name}」を削除しました。"
-  end
-
-  def new
-    @task = Task.new
-  end
-
-  def create
-    task = Task.new(task_params)
-    task.save
-    redirect_to tasks_url, notice: "タスク「#{task.name}」を登録しました。"
   end
 
   private
