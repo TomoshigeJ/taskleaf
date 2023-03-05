26c191fb539fdfef32e509dc4d6d4def153465a1
Tomoshige Jimu <keririnkery.kery@gmail.com>
2023-03-05 21:27:56 +0900

    Add validate_to_task.rb

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,6 +3,7 @@ class Task < ApplicationRecord
   validates :name, length: { maximum: 30 }
 
   validate :validate_name_not_including_comma
+  validate :validate_origin
 
   belongs_to :user
 
@@ -12,6 +13,10 @@ class Task < ApplicationRecord
 
   def validate_name_not_including_comma
     errors.add(:name, 'にカンマを含めることはできません') if name&.include?(',')
+  end
+
+  def validate_origin
+    errors.add(:name, 'じょんという名前は使うのは禁止です') if name&.include?('じょん')
   end
 
 end
